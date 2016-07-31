5741a810a28b257938ad6fcad8777c6e9f2103aa
Konstantinos Kanellis <kkanelli@hotmail.com>
2016-07-31 23:47:53 +0300

    add solutions for week 4

--- w4-nn-learning/ex4/nnCostFunction.m
+++ w4-nn-learning/ex4/nnCostFunction.m
@@ -8,8 +8,8 @@ function [J grad] = nnCostFunction(nn_params, ...
 %   [J grad] = NNCOSTFUNCTON(nn_params, hidden_layer_size, num_labels, ...
 %   X, y, lambda) computes the cost and gradient of the neural network. The
 %   parameters for the neural network are "unrolled" into the vector
-%   nn_params and need to be converted back into the weight matrices. 
-% 
+%   nn_params and need to be converted back into the weight matrices.
+%
 %   The returned parameter grad should be a "unrolled" vector of the
 %   partial derivatives of the neural network.
 %
@@ -24,8 +24,8 @@ Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):en
 
 % Setup some useful variables
 m = size(X, 1);
-         
-% You need to return the following variables correctly 
+
+% You need to return the following variables correctly
 J = 0;
 Theta1_grad = zeros(size(Theta1));
 Theta2_grad = zeros(size(Theta2));
@@ -34,11 +34,37 @@ Theta2_grad = zeros(size(Theta2));
 % Instructions: You should complete the code by working through the
 %               following parts.
 %
+
+%% Convert y values to binary vectors
+y_mat = zeros(m, num_labels);
+for i=1:m
+    y_mat(i, y(i)) = 1;
+end
+
+%% Calculate the activations
+a1 = [ ones(m, 1) X ];  % Input layer
+
+z2 = a1 * Theta1';
+a2 = [ ones(m, 1) sigmoid(z2)];  % Hidden layer
+
+z3 = a2 * Theta2';
+a3 = h = sigmoid(z3);  % Output layer
+
 % Part 1: Feedforward the neural network and return the cost in the
 %         variable J. After implementing Part 1, you can verify that your
 %         cost function computation is correct by verifying the cost
 %         computed in ex4.m
 %
+
+%% Calculate the unregularized cost function
+for i=1:m
+    J +=  sum( y_mat(i, :) * log( h(i, :)' )  +  (1 - y_mat(i,:)) * log(1 - h(i, :)' ) );
+end
+J /= (-m);
+
+%% Add the regularization term
+J += (lambda / (2*m)) * ( sum( sum( Theta1(:, 2:end) .^ 2 )) + sum( sum( Theta2(:, 2:end) .^ 2 )) );
+
 % Part 2: Implement the backpropagation algorithm to compute the gradients
 %         Theta1_grad and Theta2_grad. You should return the partial derivatives of
 %         the cost function with respect to Theta1 and Theta2 in Theta1_grad and
@@ -46,14 +72,31 @@ Theta2_grad = zeros(size(Theta2));
 %         that your implementation is correct by running checkNNGradients
 %
 %         Note: The vector y passed into the function is a vector of labels
-%               containing values from 1..K. You need to map this vector into a 
+%               containing values from 1..K. You need to map this vector into a
 %               binary vector of 1's and 0's to be used with the neural network
 %               cost function.
 %
 %         Hint: We recommend implementing backpropagation using a for-loop
-%               over the training examples if you are implementing it for the 
+%               over the training examples if you are implementing it for the
 %               first time.
 %
+
+Delta1 = zeros( size(Theta1) );
+Delta2 = zeros( size(Theta2) );
+
+for i=1:m
+    % Calculate delta3 and delta2
+    delta3 = ( a3(i, :) - y_mat(i, :) )';
+    delta2 = (Theta2' * delta3)(2:end) .* sigmoidGradient(z2(i, :)');
+
+    % Calculate Delta
+    Delta1 += delta2 * a1(i, :);
+    Delta2 += delta3 * a2(i, :);
+
+% Compute gradient from Deltas
+Theta1_grad = Delta1 ./ m;
+Theta2_grad = Delta2 ./ m;
+
 % Part 3: Implement regularization with the cost function and gradients.
 %
 %         Hint: You can implement this around the code for
@@ -62,24 +105,8 @@ Theta2_grad = zeros(size(Theta2));
 %               and Theta2_grad from Part 2.
 %
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+Theta1_grad(:, 2:end) += (lambda / m) * Theta1(:, 2:end);
+Theta2_grad(:, 2:end) += (lambda / m) * Theta2(:, 2:end);
 % -------------------------------------------------------------
 
 % =========================================================================
